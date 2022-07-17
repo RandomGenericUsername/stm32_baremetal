1b337f4b4336e920fea43ad9d6aa8ca54269c8be
Juan David <jdavidth.01@gmail.com>
2022-07-17 12:12:46 -0500

    still going with usart library

--- primer/Debug/Src/main.d
+++ primer/Debug/Src/main.d
@@ -1,5 +1,5 @@
 Src/main.o: ../Src/main.cpp ../Inc/Drivers/basicTimer.hh \
- ../Inc/DEFINITIONS.h \
+ ../Inc/DEFINITIONS.h ../Inc/MACROS.h ../Inc/DEFINITIONS.h \
  ../Inc/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
  ../Inc/Drivers/CMSIS/Include/core_cm4.h \
  ../Inc/Drivers/CMSIS/Include/cmsis_version.h \
@@ -7,9 +7,11 @@ Src/main.o: ../Src/main.cpp ../Inc/Drivers/basicTimer.hh \
  ../Inc/Drivers/CMSIS/Include/cmsis_gcc.h \
  ../Inc/Drivers/CMSIS/Include/mpu_armv7.h \
  ../Inc/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- ../Inc/Drivers/GPIO.hh ../Inc/MACROS.h ../Inc/DEFINITIONS.h \
- ../Inc/Drivers/USART.hh ../Inc/Drivers/GPIO.hh ../Inc/Drivers/ADC.hh
+ ../Inc/Drivers/GPIO.hh ../Inc/Drivers/USART.hh ../Inc/Drivers/GPIO.hh \
+ ../Inc/Drivers/ADC.hh
 ../Inc/Drivers/basicTimer.hh:
+../Inc/DEFINITIONS.h:
+../Inc/MACROS.h:
 ../Inc/DEFINITIONS.h:
 ../Inc/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
 ../Inc/Drivers/CMSIS/Include/core_cm4.h:
@@ -19,8 +21,6 @@ Src/main.o: ../Src/main.cpp ../Inc/Drivers/basicTimer.hh \
 ../Inc/Drivers/CMSIS/Include/mpu_armv7.h:
 ../Inc/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
 ../Inc/Drivers/GPIO.hh:
-../Inc/MACROS.h:
-../Inc/DEFINITIONS.h:
 ../Inc/Drivers/USART.hh:
 ../Inc/Drivers/GPIO.hh:
 ../Inc/Drivers/ADC.hh:
